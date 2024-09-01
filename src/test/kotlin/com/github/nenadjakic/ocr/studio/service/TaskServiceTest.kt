05903ffa7eee3c9bc21e07f801fd77ef07271e93
Nenad Jakic <nenad.jakic@gmail.com>
2024-09-01 20:21:20 +0200

    Added unit test for class TaskService.

---
+++ src/test/kotlin/com/github/nenadjakic/ocr/studio/service/TaskServiceTest.kt
@@ -0,0 +1,70 @@
+package com.github.nenadjakic.ocr.studio.service
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class TaskServiceTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun findAll() {
+    }
+
+    @Test
+    fun findById() {
+    }
+
+    @Test
+    fun findPage() {
+    }
+
+    @Test
+    fun insert() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun deleteById() {
+    }
+
+    @Test
+    fun upload() {
+    }
+
+    @Test
+    fun removeFiles() {
+    }
+
+    @Test
+    fun testUpdate() {
+    }
+
+    @Test
+    fun testUpdate1() {
+    }
+
+    @Test
+    fun testUpdate2() {
+    }
+
+    @Test
+    fun testUpdate3() {
+    }
+}
