58cd77b8a3bb2c1d054eac3a73fc0fd0ade85dc0
Nenad Jakic <nenad.jakic@gmail.com>
2024-09-01 20:41:24 +0200

    Added new property to OcrProgress. Added status to ProgressInfo. Created ProgressInfo extension method.

---
+++ src/main/kotlin/com/github/nenadjakic/ocr/studio/extension/ProgressInfoExtension.kt
@@ -0,0 +1,21 @@
+package com.github.nenadjakic.ocr.studio.extension
+
+import com.github.nenadjakic.ocr.studio.entity.OcrProgress
+import com.github.nenadjakic.ocr.studio.entity.Status
+import com.github.nenadjakic.ocr.studio.executor.ProgressInfo
+
+fun ProgressInfo.toOcrProgress(): OcrProgress {
+    val status =
+        when (this.progressInfoStatus) {
+            ProgressInfo.ProgressInfoStatus.CREATED -> Status.TRIGGERED
+            ProgressInfo.ProgressInfoStatus.IN_PROGRESS -> Status.STARTED
+            ProgressInfo.ProgressInfoStatus.FINISHED -> Status.FINISHED
+            ProgressInfo.ProgressInfoStatus.FAILED -> Status.FAILED
+            ProgressInfo.ProgressInfoStatus.CANCELED -> Status.CANCELED
+            ProgressInfo.ProgressInfoStatus.INTERRUPTED -> Status.INTERRUPTED
+        }
+    val progress = "$this.taskDone.toString() / $this.totalTasks.toString()"
+    val description = this.description
+
+    return OcrProgress(status, progress, description)
+}
