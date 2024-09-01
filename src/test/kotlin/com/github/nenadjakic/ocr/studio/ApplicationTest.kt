32c903aa374626155aaeb3d80e5844de24ecbc1e
Nenad Jakic <nenad.jakic@gmail.com>
2024-09-01 20:21:20 +0200

    Added unit test for class TaskService.

---
+++ src/test/kotlin/com/github/nenadjakic/ocr/studio/ApplicationTest.kt
@@ -0,0 +1,12 @@
+package com.github.nenadjakic.ocr.studio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    fun contextLoads() {
+    }
+}
